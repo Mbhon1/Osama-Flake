b825148167e7c7a7124fad951bf278ac34b6582b
Mbhon1 <mbhoni@proton.me>
2023-09-05 19:15:00 +0200

    update: symlink nvim files

--- terminal/editor/nvim/lua/plugins/dap/delve.nix
+++ terminal/editor/nvim/lua/plugins/dap/delve.nix
@@ -1,21 +1,6 @@
 {
-    home.file.".config/nvim/lua/dap/delve.lua".text = ''
-      local dap = require "dap"
-dap.adapters.go = {
-  type = "executable",
-  command = "node",
-  args = { vim.fn.stdpath "data" .. "/mason/packages/go-debug-adapter/extension/dist/debugAdapter.js" },
-}
-dap.configurations.go = {
-  {
-    type = "go",
-    name = "Debug",
-    request = "launch",
-    showLog = false,
-    program = "${file}",
-    dlvToolPath = vim.fn.stdpath "data" .. "/mason/packages/delve/dlv",
-  },
-}
-      '';
+    home.file.".config/nvim/lua/dap/delve.lua" = {
+      source = ./delve.lua;
+    };
 
 }
--- terminal/editor/nvim/lua/plugins/dap/js-debug.nix
+++ terminal/editor/nvim/lua/plugins/dap/js-debug.nix
@@ -1,25 +1,5 @@
 {
-    home.file.".config/nvim/lua/dap/js-debug.lua".text = ''
-      require("dap").adapters["pwa-node"] = {
-  type = "server",
-  host = "localhost",
-  port = "${port}",
-  executable = {
-    command = "node",
-    -- 💀 Make sure to update this path to point to your installation
-    args = { vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js", "${port}" },
-  },
-}
-
-require("dap").configurations.javascript = {
-  {
-    type = "pwa-node",
-    request = "launch",
-    name = "Launch file",
-    program = "${file}",
-    cwd = "${workspaceFolder}",
-  },
-}
-      '';
-
+    home.file.".config/nvim/lua/dap/js-debug.lua" = {
+      source = ./js-debug.lua;
+    };
 }
